0ddab94d5eb9cee834930bcf1dcc8443f7c65b18
FaceFactor <vinsenvl@berkeley.edu>
2017-11-18 06:47:14 +0000

    [chore] Split the availability in two attributes. Everything still works.

--- app/helpers/time_helper.rb
+++ app/helpers/time_helper.rb
@@ -4,5 +4,5 @@ module TimeHelper
         start_hour = range[0..1].to_i
         end_hour = range[6..7].to_i
         (((start_hour-1)%12)+1).to_s + range[2..4] + (start_hour<12?' a.m. - ':' p.m. - ') + (((end_hour-1)%12)+1).to_s + range[8..10] + (end_hour<12?' a.m.':' p.m.')
-    end
+    end 
 end
